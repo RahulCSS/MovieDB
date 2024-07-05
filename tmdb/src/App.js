1de1eb219e5ca25b629b4ef54a9d8fafeb3bfd36
Rahulcss <Rahulcs1618@gmail.com>
2024-07-05 18:02:40 +0530

    Initialization

---
+++ tmdb/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Movie App</h1>
+        <Home></Home>
+      </header>
+    </div>
+  );
+}
+
+export default App;
